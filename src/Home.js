5d7826b03c5bb55f3ffb758b78d68d470054d4ec
Christopher Kim <haxodrat@icloud.com>
2024-07-03 14:50:09 -0700

    Custom Hooks, React Router, Exact Match Routes, Router Links, useEffect clean, Route Params, Reusing Custom Hooks

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,8 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
-import { Step } from '@mui/material';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+    const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     //const [name, setName] = useState('mario');
 
@@ -14,26 +11,7 @@ const Home = () => {
     //     setBlogs(newBlogs);
     // };
 
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) {
-                        throw Error('could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
-                })
-            }, 1000);
-    }, []);
+    
 
     return ( 
         <div className='home'>
