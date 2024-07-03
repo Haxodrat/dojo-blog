dde719808800d3191eb0069fdc29afbaaa19bb1c
Christopher Kim <haxodrat@icloud.com>
2024-07-02 20:17:37 -0700

    Make it look nice and output a usestate array

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-    //let name = 'mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(4);
-
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30);
-    }
-
+    const [blogs, setBlogs] = useState([
+        { title: 'How to build a website', body: 'Hi, welcome to . . .', author: 'mario', id: 1 },
+        { title: 'Welcome Blog', body: 'Hi, welcome to . . .', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'Hi, welcome to . . .', author: 'mario', id: 3 }
+    ]);
     return ( 
         <div className='home'>
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old.</p>
-            <button onClick={handleClick}>Click</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
